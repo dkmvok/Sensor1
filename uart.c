4ee2ad23b25094bd80829902a0163704cb66186f
dkmvok <46326683+dkmvok@users.noreply.github.com>
2020-01-30 02:52:21 -0600

    Add files via upload

---
+++ uart.c
@@ -0,0 +1,138 @@
+#include <msp430.h> 
+#include <stdio.h>
+#include <stdint.h>
+#include "uart.h"
+
+/*
+ * main.c
+ */
+//2400, 4800, 9600
+
+int messageLength = 10;
+const unsigned long clkHZ = 16000000;
+const unsigned baudRate = 9600;
+static const struct baudRates baudRateTable[] =  {
+		{2400, 4800, 9600}
+	
+};
+
+
+char message[messageLength] = 0;
+int main(void) {
+
+    WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
+    BCSCTL = CALBC1_16MHZ;
+    DCOCTL = CALDCO_16MHZ;
+
+    uartInit();
+
+    unsigned char = ch;
+
+    print("TestIO\n\r");
+    putString((char *)"Testing\n");
+    ch = getChar();
+    putChar(ch);
+    putString((char *)"Input String: =\n");
+
+    	while(1) {
+
+    		getString(message,messageLength);
+    		putString(message);
+
+
+    	}
+	
+	return 0;
+}
+
+void uartInit(void) {
+
+	P1DIR |= BIT0;
+	P1OUT |= BIT0;
+
+	P1SEL = BIT1 + BIT2;
+	P1SEL2 = BIT1 + BIT2;
+
+	UCA0CTL1 |= UCSSEL_2;
+	UCA0BR0 = (baudRate >> 12) & 0XFF;
+	UCA0BR1 = (baudRate >> 4) & 0XFF;
+	UCA0MCTL = ((baudRate << 4) & 0xF0) | UCOS16;
+	UCA0CTL1 &= ~UCSWRT;
+	IE2 |= UCA0RXIE;
+
+
+	didRecieve = 0;
+	didTransmit = 0;
+
+	return;
+
+}
+
+
+unsigned char getChar(void) {
+
+	unsigned char rxChar;
+
+	while(!(IFG2 & UCA0RXIFG));
+		rxChar = UCA0RXBUF;
+
+		return rxChar;
+
+}
+
+
+void getString(char *rxMessage, int length) {
+
+	unsigned int i = 0;
+
+	while(i < length) {
+
+		*rxMessage = getChar();
+		if( *rxMessage == '\r') {
+
+				while(i < length) {
+
+					*rxMessage = '\0';
+					i++;
+
+				}
+				break;
+	   }
+	*rxMessage++;
+
+   }
+	return;
+}
+
+void putChar(unsigned ch) {
+
+	while(!IFG2 & UCA0TXIFG));
+		UCA0TXBUF = ch;
+
+	return c;
+
+}
+
+void putString(unsigned char *txMessage) {
+
+	unsigned int i = 0;
+	unsigned char ch;
+
+	while(*txMessage != '\0'){
+		ch = putChar(*txMessage++);
+
+		if( ch == '\n') {
+
+		    putChar('\r');
+
+		}
+
+	}
+	return;
+}
+
+
+
+
+
+
